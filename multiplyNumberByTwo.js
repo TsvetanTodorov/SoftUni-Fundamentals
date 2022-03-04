f1ca6ab786e4637f31cd6d52f3d6bb14c76d73f2
Tsvetan Todorov <tsvetantodorov@Tsvetans-Air.lan>
2022-03-04 14:27:38 +0200

    Adding a new folder

---
+++ multiplyNumberByTwo.js
@@ -0,0 +1,6 @@
+function multiplyNumberByTwo(number) {
+    let result = number * 2;
+    console.log(result);
+
+}
+multiplyNumberByTwo(["5"])
---
+++ numbersFromNtoOne.js
@@ -0,0 +1,10 @@
+function numbersFromNToOne(number) {
+
+
+    while (number >= 1) {
+        console.log(number);
+        number--
+    }
+
+}
+numbersFromNToOne(5)
